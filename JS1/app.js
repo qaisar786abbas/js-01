fe564997e2e4c53d658691f2cc54a22bebd17ce4
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-02-03 12:16:32 +0500

    added files

---
+++ JS1/app.js
@@ -0,0 +1,44 @@
+// ### chapter 1 ###
+
+// Question No 01
+// alert("HELLO, Sir");
+
+
+//question No 02
+// alert("Error! Please enter a valid Password");
+
+
+//question No 03
+// alert("Welcome to JS land...\n HAPPY CODING!");
+
+//Question No 04 (a)
+// alert("welcome to JS land...");
+
+
+//Question No 04 (b)
+//alert("Happy coding");
+
+
+//Question No 05
+// alert("Hello...I can run JS through my web browser's console");
+<script>var alert("Hello...i can run JS through my web browser's console");
+console.log(alert);</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
